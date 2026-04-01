b65b57e6b15a2a85359f5b3fbf1d67173813975a
ant1Da <antoine.dast@edouardgand.fr>
2026-04-01 13:58:36 +0200

    Premier commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TP_Serialisation___Gestions_d_une_liste_de_jeux_vidéo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
